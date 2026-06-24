811fc7722f3be60ea442bd38f6d30cbefee22b18
Md Abdur Rahman <arsir.du@gmail.com>
2026-06-24 12:03:06 +0600

    Restore app - landing only at /landing

--- src/App.js
+++ src/App.js
@@ -273,35 +273,25 @@ function AppShell() {
 
 function AppContent() {
   const { user } = useAuth();
-  return user ? <AppShell /> : <Login />;
+
+  if (!user) return <Login />;
+
+  return <AppShell />;
 }
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
-
           <Route path="/" element={<Landing />} />
-          <Route path="/landing" element={<Landing />} />
-
-          <Route
-            path="/app"
-            element={
-              <AuthProvider>
-                <AppContent />
-              </AuthProvider>
-            }
-          />
-
           <Route path="/signup" element={<Signup />} />
-
+          <Route path="/app" element={<AppContent />} />
           <Route path="*" element={<Landing />} />
-
         </Routes>
       </BrowserRouter>
 
       <Analytics />
-    </>
+    </AuthProvider>
   );
 }
